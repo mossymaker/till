fce7c3a91559a5eacfc28c3cd13505ae53a0da64
Patrick Walsh <patrick@craftycorvid.com>
2016-08-31 12:29:39 -0700

    It blinks! Adding super basic main loop; updating makefile

--- firmware/main.c
+++ firmware/main.c
@@ -5,12 +5,18 @@
  */
 
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(void)
 {
-    /* insert your hardware initialization here */
-    for(;;){
-        /* insert your main loop code here */
-    }
-    return 0;   /* never reached */
+  // Configure PB5 as an output
+  DDRB = 0b00100000;
+
+  while(1) {
+    PORTB = 0b00100000; // Turn on PB5
+    _delay_ms(750);
+
+    PORTB = 0b00000000; // Turn off PB5
+    _delay_ms(750);
+  }
 }
